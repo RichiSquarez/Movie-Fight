8ea441cec75d3a3a2ce0cbabab58099995cdce5d
RichiSquarez <natsudlareklami@gmail.com>
2024-06-18 16:39:06 +0200

    Added methods to close dropdown menu. Added response for user click on movie.

--- app.js
+++ app.js
@@ -7,26 +7,39 @@ input1.addEventListener("input", debounce(async(k) => {
     if(k.target.value.length > 0){
         try{
             const dropdown = document.querySelector(".first-search-dropdown")
-
-            const response = await getMovies(k.target.value);
+            dropdown.innerHTML = '';
             dropdown.style.display = "flex";
+            const response = await getMovies(k.target.value);
+
             for(let movie of response){
                 const div = document.createElement("div");
+                const imageSRC = movie.Poster === 'N/A' ? '' : movie.Poster;
                 div.classList.add("first-search-dropdown-item");
 
                 div.innerHTML = `
-                    <img src="${await movie.Poster}" height="80">
+                    <img src="${imageSRC}" height="80">
                     <p>${await movie.Title} (${await movie.Year})</p>
                 `
 
-                document.querySelector(".first-search-dropdown").append(div);
+                div.addEventListener("click", (e) => {
+                    document.querySelector(".first-search-dropdown").style.display = "none";
+                    input1.value = movie.Title;
+                })
+
+                dropdown.append(div);
 
             }
         }
         catch (e) {
             console.error(e.message);
         }
+    }else{
+        document.querySelector(".first-search-dropdown").style.display = "none";
     }
 }));
 
-
+document.addEventListener("click", (e) => {
+    if(!document.querySelector(".first-search-dropdown").contains(e.target)){
+        document.querySelector(".first-search-dropdown").style.display = 'none';
+    }
+})
